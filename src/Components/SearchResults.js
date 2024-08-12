2e3aed6928b5dde4cf5ef0054df320a09a957472
ManojN22 <manojkumar22072001@gmail.com>
2024-08-12 14:08:28 -0400

    donetada done

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,9 +1,11 @@
+import Loader from "./Loading.js";
 import SearchResultCard from "./SearchResults.Cards"
 import { useSelector } from "react-redux";
 
 export default function SearchResult(){
+    const bookStoreLoader = useSelector(state =>state.bookStore.status);
     const bookStore = useSelector((state) => state.bookStore.data);
     return <div className="Search-Results">
-    {bookStore.map(data=><SearchResultCard searchResultCard = {data}/>)}
+    {bookStoreLoader === 'loading'?<Loader/>:bookStore.map(data=><SearchResultCard searchResultCard = {data}/>)}
     </div>
 }
